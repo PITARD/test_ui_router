ecc67dc80cbb2f936fc6b1f06f9022cd2899599c
Alexandre Cadeau <alexandre.cadeau@etu.univ-nantes.fr>
2016-05-26 18:11:57 +0200

    Ajout du niveau 2 (graph => arbre)

--- dist/bundle.js
+++ dist/bundle.js
@@ -12,24 +12,24 @@ angular.module('cyrcleoflifeApp', ['ui.router']).config(function ($stateProvider
     template: '<div>You are borned<br><button ui-sref="birth.childhood">childhood</button><div ui-view></div></div>'
   }).state('birth.childhood', {
     url: '/childhood',
-    template: '<div>You are grown up<br><button ui-sref="birth.childhood.study">study</button><br><button ui-sref="birth.childhood.career">career</button><div ui-view></div></div>'
+    template: '<div>You are grown up<br><button ui-sref="birth.childhood">childhood</button><div ui-view></div></div>'
   }).state('birth.childhood.study', {
     url: '/study',
-    template: '<div>Really ?<br><button ui-sref="birth.childhood.study.phd">phd</button><div ui-view></div></div>'
+    template: '<div>Really ?<br><button ui-sref="birth.childhood">childhood</button><div ui-view></div></div>'
   }).state('birth.childhood.study.phd', {
     url: '/phd',
-    template: '<div>Great, What next ?<br><button ui-sref="birth.childhood.study.phd">phd</button><div ui-view></div></div>'
+    template: '<div>Great, What next ?<br><button ui-sref="birth.childhood">childhood</button><div ui-view></div></div>'
   }).state('birth.childhood.career', {
     url: '/career',
-    template: '<div>GoodLuck !<br><button ui-sref="birth.childhood.career.workinglife">workinglife</button><div ui-view></div></div>'
+    template: '<div>GoodLuck !<br><button ui-sref="birth.childhood">childhood</button><div ui-view></div></div>'
   }).state('birth.childhood.career.workinglife', {
     url: '/workinglife',
-    template: '<div>Hard Work !<br><button ui-sref="birth.childhood.career.workinglife.pension">pension</button><div ui-view></div></div>'
+    template: '<div>Hard Work !<br><button ui-sref="birth.childhood">childhood</button><div ui-view></div></div>'
   }).state('birth.childhood.career.workinglife.pension', {
     url: '/pension',
-    template: '<div>Good old days<br><button ui-sref="birth.childhood.career.workinglife.pension.death">death</button><div ui-view></div></div>'
+    template: '<div>Good old days<br><button ui-sref="birth.childhood">childhood</button><div ui-view></div></div>'
   }).state('birth.childhood.career.workinglife.pension.death', {
-    url: '/death',
+    url: '/pension',
     template: '<div>RIP<br><button ui-sref="birth">birth</button></div>'
   });
 });
